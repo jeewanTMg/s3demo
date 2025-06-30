7c830b57d6fc48dc60c39f8bd08a2c79ffeffcab
jeewan magar <jeewantmgr@gmail.com>
2025-06-29 22:55:37 -0500

    Initialize commit

---
+++ src/main/java/com/demos3/Demos3Application.java
@@ -0,0 +1,13 @@
+package com.demos3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demos3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demos3Application.class, args);
+    }
+
+}
